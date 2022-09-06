c0cd4a5fee723d1f95b2f5afddfe1d7766802340
Vkirovpz <Vkirovpz@students.softuni.bg>
2022-09-06 23:34:35 +0300

    Create HTML Table for All Students View

--- studentadminportal-ui/src/app/students/students.component.ts
+++ studentadminportal-ui/src/app/students/students.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Student } from '../models/ui-models/student.model';
 import { StudentService } from './student.service';
 
 @Component({
@@ -8,6 +10,11 @@ import { StudentService } from './student.service';
 })
 export class StudentsComponent implements OnInit {
 
+  students: Student[] = [];
+  displayedColumns: string[] = ['firstName','lastName','dateOfBirth','email','mobile','gender'];
+  dataSource: MatTableDataSource<Student> = new MatTableDataSource<Student>();
+
+
   constructor(private studentService : StudentService) { }
 
   ngOnInit(): void {
@@ -15,8 +22,8 @@ export class StudentsComponent implements OnInit {
     .subscribe(
       (successResponse) =>
       {
-        console.log(successResponse[0].firstName);
-        console.log(successResponse[0].lastName);
+        this.students = successResponse;
+        this.dataSource = new MatTableDataSource<Student>(this.students);
       },
       (errorResponse) => {
         console.log(errorResponse);
