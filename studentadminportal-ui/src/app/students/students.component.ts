1f47b541b5f9ef18cf6998b7f115ab7cd55d18c8
Vkirovpz <Vkirovpz@students.softuni.bg>
2022-09-06 23:49:31 +0300

    Create paginator

--- studentadminportal-ui/src/app/students/students.component.ts
+++ studentadminportal-ui/src/app/students/students.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Student } from '../models/ui-models/student.model';
 import { StudentService } from './student.service';
@@ -13,6 +14,7 @@ export class StudentsComponent implements OnInit {
   students: Student[] = [];
   displayedColumns: string[] = ['firstName','lastName','dateOfBirth','email','mobile','gender'];
   dataSource: MatTableDataSource<Student> = new MatTableDataSource<Student>();
+  @ViewChild(MatPaginator) matPaginator!: MatPaginator;
 
 
   constructor(private studentService : StudentService) { }
@@ -24,6 +26,11 @@ export class StudentsComponent implements OnInit {
       {
         this.students = successResponse;
         this.dataSource = new MatTableDataSource<Student>(this.students);
+
+        if(this.matPaginator)
+        {
+          this.dataSource.paginator = this.matPaginator;
+        }
       },
       (errorResponse) => {
         console.log(errorResponse);
