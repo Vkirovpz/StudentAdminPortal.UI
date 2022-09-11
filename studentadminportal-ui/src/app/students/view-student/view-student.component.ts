1154c1119797097b7436dbc058759000de9183be
Vkirovpz <Vkirovpz@students.softuni.bg>
2022-09-11 21:58:13 +0300

    Implement Update Student Logic

--- studentadminportal-ui/src/app/students/view-student/view-student.component.ts
+++ studentadminportal-ui/src/app/students/view-student/view-student.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
+import { Gender } from 'src/app/models/ui-models/gender.model';
 import { Student } from 'src/app/models/ui-models/student.model';
+import { GenderService } from 'src/app/services/gender.service';
 import { StudentService } from '../student.service';
 
 @Component({
@@ -30,9 +33,13 @@ export class ViewStudentComponent implements OnInit {
       postalAddress: ''
     }
   }
+  genderList: Gender[] = [];
+
 
   constructor(private readonly studentService: StudentService,
-    private readonly route: ActivatedRoute) { }
+    private readonly route: ActivatedRoute,
+    private readonly genderService: GenderService,
+    private snackbar: MatSnackBar) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(
@@ -47,8 +54,31 @@ export class ViewStudentComponent implements OnInit {
               this.student = successResponce;
             }
           );
+
+          this.genderService.getGenderList()
+          .subscribe(
+            (successResponse) => {
+              this.genderList = successResponse;
+            }
+          );
         }
       }
     );
+  }
+
+  onUpdate(): void{
+    this.studentService.updateStudent(this.student.id, this.student)
+    .subscribe(
+      (successResponse) => {
+      this.snackbar.open('Student updated successfully', undefined, {
+        duration: 2000
+      });
+
+      },
+      (errorResponse) => {
+
+      }
+    );
+
   }
 }
